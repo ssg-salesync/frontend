1b7e5c2b9a213dca834af8fbcfea6100fcc0338f
rlatkd <rlatkdgns042@naver.com>
2023-12-08 19:37:58 +0900

    feat: login-design login 페이지 디자인 수정

--- src/components/pos/PosItem.js
+++ src/components/pos/PosItem.js
@@ -1,11 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ItemContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 300px; /* 좌우 길이 조정 */
+    margin-bottom: 10px;
+`;
+
+const Label = styled.label`
+    margin-right: 10px;
+`;
+
+const InputField = styled.input`
+    flex-grow: 1;
+`;
+
 function PosItem() {
     return (
-        <>
-            <h1>POS아이템컴포넌트입니다.</h1>
-            품목명<input type="text"/>
-            가격<input type="text"/>
-        </>
+        <ItemContainer>
+            <div>
+                <Label htmlFor="itemName">품목명:</Label>
+                <InputField type="text" id="itemName" />
+                <br/>
+                <Label htmlFor="itemPrice">가격:</Label>
+                <InputField type="text" id="itemPrice" />
+            </div>
+        </ItemContainer>
     );
-};
+}
 
-export default PosItem
+export default PosItem;
