12ca236bd0b8cb545ba527eced8dc40a784fac15
minju <ruri9898@gmail.com>
2023-12-08 10:55:58 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-// import { useState } from 'react';
 import './App.css'
 // import GlobalStyle from './styles/GlobalStyle';
-// import Button from 'react-bootstrap/Button';
 // import { Reset } from "styled-reset"
-// import GlobalStyle from "./styles/GlobalStyle";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Routing from './routes/Routing';
 
-
 function App() {
   return (
     <>
+      {/* <Reset/> */}
       {/* <GlobalStyle/>  */}
-    {/* <OrderModal show={orderModalOn} onHide={()=>setOrderModalOn(false)}/> */}
-    <Header/>
-    <main>
-      <div>
-        <Routing/>
-      </div>
-    </main>
-    <Footer/>    
-{/*       
-      <Reset/>
-      <GlobalStyle/> */}
-
-       
-
-
+      <Header/>
+      <main>
+        <div>
+          <Routing/>
+        </div>
+      </main>
+      <Footer/>    
     </>
   );
 };
