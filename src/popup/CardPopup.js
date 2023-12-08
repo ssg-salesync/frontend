b2bf445b5d617b4c563a137a5af5f018092ae7d7
minju <ruri9898@gmail.com>
2023-12-08 23:53:03 +0900

    feat:CardPopup

--- src/popup/CardPopup.js
+++ src/popup/CardPopup.js
@@ -16,12 +16,31 @@ const modalStyle ={
         flexDirection: 'row',
       },
 };
+
 const Button = styled.button`
     border: none;
     padding: 0;
     width: 100%;
     height: 100%;
 `
+const CardDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items : center;
+    width: '100%';
+    height: '100%';
+`
+const PayIngDiv = styled.div`
+    background: gray;
+    width: 60%;
+    height: 50%;
+    display: flex;
+    align-items : center;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+`
+
 function CardPopup() {
     const [cardModalOn, setCardModalOn] = useState(false);
 
@@ -38,7 +57,12 @@ function CardPopup() {
         <Button type='button' onClick={openModal}>카드</Button>
         <Modal isOpen={cardModalOn} onRequestClose={closeModal} style={modalStyle} contentLabel="Example Modal">
             {/* <ModalContent/> */}
-            <p> 결제중 </p>
+            <CardDiv style={{width: '100%', height: '100%', }}>
+            <PayIngDiv>
+                <div> 카드를 넣어주세요 </div>
+                <div> ~결제중~ </div>
+            </PayIngDiv>
+            </CardDiv>
         </Modal>
         </>
     );
