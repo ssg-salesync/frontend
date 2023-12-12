a5c11afb5ea0c0cefbee0f532171e6fe79bda424
rlatkd <rlatkdgns042@naver.com>
2023-12-12 10:10:49 +0900

    feat: PosPage-design PosPage 기초 기능 구현 완료

--- src/components/pos/PosItem.js
+++ src/components/pos/PosItem.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
+const ItemDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: calc((100vh - 40px) / 2);
+  padding: 20px 0;
+`;
+
 const ItemContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    width: 300px; /* 좌우 길이 조정 */
-    margin-bottom: 10px;
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  transition: transform 0.3s;
+  cursor: pointer;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  &:hover {
+    transform: scale(1.1) rotate(2deg);
+  }
 `;
 
-const Label = styled.label`
-    margin-right: 10px;
-`;
-
-const InputField = styled.input`
-    flex-grow: 1;
+const PlusContainer = styled(ItemContainer)`
+  font-size: 50px;
+  margin-right: 0;
 `;
 
 function PosItem() {
-    return (
-        <ItemContainer>
-            <div>
-                <Label htmlFor="itemName">품목명:</Label>
-                <InputField type="text" id="itemName" />
-                <br/>
-                <Label htmlFor="itemPrice">가격:</Label>
-                <InputField type="text" id="itemPrice" />
-            </div>
+  const [containers, setContainers] = useState([]);
+
+  const handleAddContainer = () => {
+    const newContainers = [...containers];
+    newContainers.unshift({});
+    setContainers(newContainers);
+  };
+
+  return (
+    <ItemDiv>
+      {containers.map((_, index) => (
+        <ItemContainer >
+          <div>
+            <span>{index + 1}</span>
+          </div>
         </ItemContainer>
-    );
+      ))}
+      <PlusContainer onClick={handleAddContainer}>
+        <div>
+          <span>+</span>
+        </div>
+      </PlusContainer>
+    </ItemDiv>
+  );
 }
 
 export default PosItem;
