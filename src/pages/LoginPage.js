d99172a3d24aaed08aa5689fb1b913a88226ff38
rlatkd <rlatkdgns042@naver.com>
2023-12-20 11:18:37 +0900

    feat: LoginPage-api LoginPage api 연동

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
+import { LoginPostApi } from "../api/auth/LoginPostApi";
+
+/* eslint-disable */
 
 // 로그인 영역
 const LoginDiv = styled.div`
@@ -45,6 +49,7 @@ const LoginButton = styled.button`
   border-color: #289AFF;
   background-color: #289AFF;
   color: white;
+  cursor: pointer;
 `;
 
 // 회원가입 버튼
@@ -56,19 +61,60 @@ const SignupButton = styled.button`
   border-color: #289AFF;
   background-color: #289AFF;
   color: white;
+  cursor: pointer;
 `;
 
 function LoginPage() {
+
+  // 로그인에 사용한 아이디, 비밀번호 상태 저장
+  const [ loginData, setLoginData ] = useState({
+    username: '',
+    password: ''
+  })
+
+  // 로그인 버튼 클릭 이벤트핸들러
+  const handlerLoginClick = () => {
+    const data = {
+      username: loginData.username,
+      password: loginData.password
+    };
+
+    // API 통신
+    LoginPostApi(data)
+      .then(res => {
+        console.log('로그인 성공 | 토큰 정보: ', res);
+      })
+      .catch(err => {
+        console.error('로그인 실패 ㅣ 에러메세지: ', err);
+      });
+  };
+  
+  // 로그인, 회원가입칸에 입력시 데이터 상태 변경
+  const handlerInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({...loginData, [name]: value});
+  };
+
     return (
       <LoginDiv>
         <LoginDivTitle>로그인</LoginDivTitle>
         <InsertDiv>
-          <InputField type="text" placeholder="아이디"/>
+          <InputField
+            type="text"
+            name="username"
+            value={loginData.username}
+            onChange={handlerInputChange}
+            placeholder="아이디"/>
           <br/>
-          <InputField type="password" placeholder="비밀번호"/>
+          <InputField
+            type="password"
+            name="password"
+            value={loginData.password}
+            onChange={handlerInputChange}
+            placeholder="비밀번호"/>
           <br/>
             <Link to="/home">
-              <LoginButton type="submit">로그인</LoginButton>
+              <LoginButton type="submit" onClick={handlerLoginClick}>로그인</LoginButton>
             </Link>
             <Link to="/signup">
               <SignupButton type="submit">회원가입</SignupButton>
