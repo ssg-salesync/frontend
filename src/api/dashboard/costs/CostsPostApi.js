e4a411402d1bf8b6bffe269b2f3b9b648370df14
Kwak-Minju <ruri9898@gmail.com>
2024-01-19 16:02:28 +0900

    feat/cost api 수정

--- src/api/dashboard/costs/CostsPostApi.js
+++ src/api/dashboard/costs/CostsPostApi.js
@@ -9,7 +9,7 @@ export async function CostsPostApi(items){
     const headers = Token();
 
     try{
-        await axios.post(`${URL}/dashboard/costs`, items, {headers}).then(res => {
+        await axios.post(`${URL}/bff/costs`, items, {headers}).then(res => {
             console.log(res.data);
             alert('원가를 등록했습니다.')
             return res;
