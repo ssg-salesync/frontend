1d7cac77ab405fd8481ae1b4df61cf4527a12d90
Kwak-Minju <ruri9898@gmail.com>
2024-01-22 09:29:54 +0900

    border

--- src/components/order/OrderList.js
+++ src/components/order/OrderList.js
@@ -6,7 +6,7 @@ const ListTable = styled.table`
     width : 100%;
     // height : 8%;
     text-align: center;
-    border-collapse: separate;
+    border-collapse: collapse;
     border-spacing: 0 10px; /* 수평 간격은 0, 수직 간격은 10px로 설정 */
 `
 const ListTbody = styled.tbody`
@@ -16,10 +16,13 @@ const ListTbody = styled.tbody`
 const ListTr = styled.tr`
     height: 2.5rem;
     white-space: nowrap;
+    border: 1px solid #ddd;
+    background: #FFF;
+    border-radius: 0.3rem;
 
     &:not(:last-child) {
         margin: 1rem; /* 마지막 행을 제외한 모든 행에 간격을 주는 부분 */
-        background: #E4F4FF;
+        // background: #E4F4FF;
     }
 
     // 반응형에 맞게 폰트 크기 조정
@@ -54,7 +57,7 @@ function OrderList(){
             {menu.map((category) => (
                 category.items.map((item) => (
                     item.quantity !== 0 && (
-                    <ListTr key={item.item_id} style={{background: '#FFF'}}>
+                    <ListTr key={item.item_id}>
                         <ListTd>{category.category_name}</ListTd>
                         <ListTd>{item.name}</ListTd>
                         {/* <ListTd>{item.price}원</ListTd> */}
